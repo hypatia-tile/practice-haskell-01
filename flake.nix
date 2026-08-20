a8e6b7a06f477df5c9cbdff5d29bbc9703b408c5
shinokun <hypatia.tile02021@gmail.com>
2026-08-20 12:54:27 +0900

    feat: set Haskell development environment

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "Haskell development environment for the experiment";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        hpkgs = pkgs.haskellPackages;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            (hpkgs.ghcWithPackages (p: [
+              p.hoogle
+              p.transformers
+              p.bytestring
+              p.megaparsec
+            ]))
+            hpkgs.cabal-install
+            hpkgs.haskell-language-server
+            hpkgs.fourmolu
+            hpkgs.fast-tags
+          ];
+        };
+      }
+    );
+}
